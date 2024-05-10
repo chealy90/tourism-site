1c136afdad74194ba028cc1a0f4e00157bebd22b
Christopher Healy <christopherhealy20@gmail.com>
2024-05-10 14:11:13 +0100

    table layout and basic display completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,55 @@
-const URL = "https://derek.comp.dkit.ie/java_script/example_code/dublin_attractions.json"
+const URL = "landmarks.json"
 let data
+let keys
 
 window.onload = () => {
     fetch(URL)
     .then(rawData => rawData.json())
     .then(jsonData => {
         data = jsonData
-    })
-    
-    console.log(data)
+        keys = Object.keys(data[0])
+        displayTable()
+    })  
 }
 
+function displayTable(){
+    //headers
+    let htmlString = `<table><tr>`
+    keys.forEach(key => {
+        htmlString += `<th>${capitaliseFirstLetter(key)}</th>`
+    })
+    htmlString += `</tr>`
+
+    //table rows
+    data.forEach(row => {
+        htmlString += `<tr>`
+        keys.forEach(key=> {
+            if (key==="photosURLs"){
+                htmlString += `<td><img class="tableImg" src=${row[key][0]}></td>`
+            } else {
+                htmlString += `<td>${row[key]}</td>`
+            }
+            
+        })
+        htmlString += `</tr>`
+    })
+    
+
+
+        
+htmlString += `</table>`
+    document.getElementById("mainContent").innerHTML = htmlString
+}
+
+
+
+function capitaliseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+/*
+name, lat, long, address, description, phone, photos, tags, rating
 
 /*
 TODO
